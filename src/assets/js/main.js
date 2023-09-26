a57a3be9899baa6f361a865a02334d3045ea663d
Nick <nik.telenchy@gmail.com>
2023-09-26 20:06:45 +0300

    added full form

--- src/assets/js/main.js
+++ src/assets/js/main.js
@@ -576,6 +576,7 @@ function backArrow(){
   $('.img-btn-new').attr('src', '../assets/imgs/template/icons/arrowWhite.svg')
 }
 
+
 // Send data to server
 function onclickForm(){
   const NAME = $('#nform').val()
